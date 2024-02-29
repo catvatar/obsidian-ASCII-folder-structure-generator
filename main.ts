486699224cd4e148a46b424c1515b7407a1d4551
Olek Gogol <olekgogol@gmail.com>
2024-02-29 02:07:22 +0100

    added rendering for ```tree

--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { generateTree } from "generate-tree";
+import { parseInput } from "parse-input";
 
 interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	charset: "ascii" | "utf-8";
+	fullPath: boolean;
+	trailingDirSlash: boolean;
+	rootDot: boolean;
 }
 
 export default class MyPlugin extends Plugin {
@@ -15,95 +14,28 @@ export default class MyPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+		this.registerMarkdownCodeBlockProcessor("tree", (source, el) => {
+			const tree = generateTree(parseInput(source), {
+				charset: "utf-8",
+				fullPath: this.settings.fullPath,
+				trailingDirSlash: this.settings.trailingDirSlash,
+				rootDot: this.settings.rootDot,
+			});
+			el.createEl("pre").createEl("code", { text: tree });
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign({}, await this.loadData());
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
@@ -116,19 +48,63 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Charset")
+			.setDesc(
+				"Which set of characters to use when rendering directory lines"
+			)
+			.addDropdown((dropdown) =>
+				dropdown
+					.addOption("utf-8", "utf-8")
+					.addOption("ascii", "ascii")
+					.setValue(this.plugin.settings.charset)
+					.onChange(async (value: string) => {
+						this.plugin.settings.charset =
+							value === "utf-8" ? "utf-8" : "ascii";
+						await this.plugin.saveSettings();
+					})
+			);
+		new Setting(containerEl)
+			.setName("Full Path")
+			.setDesc(
+				"Whether or not to append trailing slashes to directories. Items that already include a trailing slash will not have another appended."
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setDisabled(false)
+					.setValue(this.plugin.settings.fullPath)
+					.onChange(async (value) => {
+						this.plugin.settings.fullPath = value;
+						await this.plugin.saveSettings();
+					})
+			);
+		new Setting(containerEl)
+			.setName("Trailing Dir Slash")
+			.setDesc("Whether or not to print the full path of the item")
+			.addToggle((toggle) =>
+				toggle
+					.setDisabled(false)
+					.setValue(this.plugin.settings.trailingDirSlash)
+					.onChange(async (value) => {
+						this.plugin.settings.trailingDirSlash = value;
+						await this.plugin.saveSettings();
+					})
+			);
+		new Setting(containerEl)
+			.setName("Root Dot")
+			.setDesc("Whether or not to render a dot as the root of the tree")
+			.addToggle((toggle) =>
+				toggle
+					.setDisabled(false)
+					.setValue(this.plugin.settings.rootDot)
+					.onChange(async (value) => {
+						this.plugin.settings.rootDot = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
